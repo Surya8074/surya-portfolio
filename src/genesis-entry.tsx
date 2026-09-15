ab6f51084c81759765a7f1ef971aa45e616e37ce
Surya8074 <99716544+Surya8074@users.noreply.github.com>
2026-09-15 22:05:52 +0530

    Disable Genesis timestamp navigation

--- src/genesis-entry.tsx
+++ src/genesis-entry.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import GenesisCaseStudy from './GenesisCaseStudy';
-import GenesisTimeline from './GenesisTimeline';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GenesisTimeline>
-      <GenesisCaseStudy />
-    </GenesisTimeline>
+    <GenesisCaseStudy />
   </React.StrictMode>,
 );
