45cd8bc657ff47b68e54e0a60bcc28e263ddc99f
Surya8074 <99716544+Surya8074@users.noreply.github.com>
2026-09-15 20:08:50 +0530

    Connect Genesis case study page

--- src/genesis-entry.tsx
+++ src/genesis-entry.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import GenesisHero from './GenesisHero';
+import GenesisCaseStudy from './GenesisCaseStudy';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GenesisHero />
+    <GenesisCaseStudy />
   </React.StrictMode>,
 );
