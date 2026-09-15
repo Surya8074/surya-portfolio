add1d5698695e4a65d60573c6171844017074433
Surya8074 <99716544+Surya8074@users.noreply.github.com>
2026-09-15 21:56:45 +0530

    Add jump navigation to Genesis case study

--- src/genesis-entry.tsx
+++ src/genesis-entry.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import GenesisCaseStudy from './GenesisCaseStudy';
+import GenesisTimeline from './GenesisTimeline';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GenesisCaseStudy />
+    <GenesisTimeline>
+      <GenesisCaseStudy />
+    </GenesisTimeline>
   </React.StrictMode>,
 );
