bae08fa8f99a5680271f895bb010a61390071752
Surya8074 <99716544+Surya8074@users.noreply.github.com>
2026-09-16 11:38:47 +0530

    Add Genesis Style 2 entry

---
+++ src/genesis-style2-entry.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import GenesisStyle2 from './GenesisStyle2';
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode><GenesisStyle2 /></React.StrictMode>,
+);
