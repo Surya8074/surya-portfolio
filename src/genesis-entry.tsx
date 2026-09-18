953526c04c3de5cdf424788b72e799187147b311
Surya8074 <99716544+Surya8074@users.noreply.github.com>
2026-09-18 16:32:51 +0530

    Restore Genesis V7 as primary case study route

--- src/genesis-entry.tsx
+++ src/genesis-entry.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import GenesisCaseStudy from './GenesisCaseStudy';
+import GenesisV7 from './GenesisV7';
+import './genesis-v7.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <GenesisCaseStudy />
+    <GenesisV7 />
   </React.StrictMode>,
 );
