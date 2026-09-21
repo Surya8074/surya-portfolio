a7d261d0bcdd8cce2f2cb8a85f0812fb5c014e8d
Surya8074 <99716544+Surya8074@users.noreply.github.com>
2026-09-21 12:58:32 +0530

    Add Genesis V8 route to Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
         genesisV5: 'work/genesis-v5/index.html',
         genesisV6: 'work/genesis-v6/index.html',
         genesisV7: 'work/genesis-v7/index.html',
+        genesisV8: 'work/genesis-v8/index.html',
       },
     },
   },
