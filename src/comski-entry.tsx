41b6b01991de44db9812414ca12d94b7a702244a
Surya8074 <99716544+Surya8074@users.noreply.github.com>
2026-09-26 13:44:07 +0530

    Add ComSki case study entrypoint

---
+++ src/comski-entry.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import ComskiCaseStudy from './ComskiCaseStudy';
+import './comski-case-study.css';
+createRoot(document.getElementById('root')!).render(<React.StrictMode><ComskiCaseStudy/></React.StrictMode>);
